b1acc48469082578c758704f1da90cd6db587216
Sharan <sharansingh9.8.6.0@gmail.com>
2025-07-31 02:28:32 +0530

    Add Auth Login/Signup and Logout functionality

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,9 @@ import * as domain from "node:domain";
 import NavBar from "~/Components/NavBar";
 import {resumes} from "../../constants";
 import ResumeCard from "~/Components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
+import {useEffect} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +15,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+
+  const {auth} = usePuterStore();
+  const navigate = useNavigate();
+
+  useEffect( () => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <NavBar/>
     <section className="main-section">
